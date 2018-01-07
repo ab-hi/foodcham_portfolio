1a7a54238d64565a762d9bfa9522ac49a924d7c7
ab-hi <bansal.abhishek101@yahoo.com>
2018-01-07 18:21:37 +0530

    rest later

--- src/components/home/home-features.js
+++ src/components/home/home-features.js
@@ -11,45 +11,46 @@ export default class Home_features extends Component{
     render(){
         return(
             <section id="site-content">
-            <div className="home-cfa">
-    <div className="container">
-        <div className="row">
-    <div className="col-sm-3 cfa-col">
-        <span><img className="img-responsive" src={img1}/></span>
-        <span className="cfa-text">Within minutes you can start placing more orders.</span>
-        <a className="cfa-link" href="download.html">Download Now <i className="fa fa-angle-right"></i></a>
-    </div>
-    <div className="col-sm-3 cfa-col">
-        <span><img className="img-responsive" src={img2}/></span>
-        <span className="cfa-text">Want to see how it feels to use FoodCham?</span>
-        <a className="cfa-link" href="demo.html">View Demo <i className="fa fa-angle-right"></i></a>
-    </div>
-    <div className="col-sm-3 cfa-col">
-        <span><img className="img-responsive" src={img3}/></span>
-        <span className="cfa-text">Become a FoodCham expert yourself and impress.</span>
-        <a className="cfa-link" href="#">Read Documentation <i className="fa fa-angle-right"></i></a>
-    </div>
-    <div className="col-sm-3 cfa-col">
-        <span><img className="img-responsive" src={img4}/></span>
-        <span className="cfa-text">Get Support & Discuss Things about FoodCham.</span>
-        <a className="cfa-link" href="#">View Forums <i className="fa fa-angle-right"></i></a>
-    </div>
-</div>    </div>
-</div>
+                <div className="home-cfa">
+                    <div className="container home-features">
+                        <div className="row">
+                            <div className="col-sm-3 cfa-col">
+                                <span><img className="img-responsive" src={img1}/></span>
+                                <span className="cfa-text">Within minutes you can start placing more orders.</span>
+                                <a className="cfa-link" href="download.html">Download Now <i className="fa fa-angle-right"></i></a>
+                            </div>
+                            <div className="col-sm-3 cfa-col">
+                                <span><img className="img-responsive" src={img2}/></span>
+                                <span className="cfa-text">Want to see how it feels to use FoodCham?</span>
+                                <a className="cfa-link" href="demo.html">View Demo <i className="fa fa-angle-right"></i></a>
+                            </div>
+                            <div className="col-sm-3 cfa-col">
+                                <span><img className="img-responsive" src={img3}/></span>
+                                <span className="cfa-text">Become a FoodCham expert yourself and impress.</span>
+                                <a className="cfa-link" href="#">Read Documentation <i className="fa fa-angle-right"></i></a>
+                            </div>
+                            <div className="col-sm-3 cfa-col">
+                                <span><img className="img-responsive" src={img4}/></span>
+                                <span className="cfa-text">Get Support & Discuss Things about FoodCham.</span>
+                                <a className="cfa-link" href="#">View Forums <i className="fa fa-angle-right"></i></a>
+                            </div>
+                        </div>    
+                    </div>
+                </div>
 
-
-<div className="container">
-    <div className="cta cta-default border-all margin-top-sm">
-        <div className="row">
-            <div className="col-sm-9 cta-text margin-top-xs">
-                <h4>We’re serving up some pretty tasty features</h4>
-            </div>
-            <div className="col-sm-3 cta-button border-sm-left text-center">
-                <Link className="btn btn-orange" to="/features"><span>See more features</span></Link>
-            </div>
-        </div>
-    </div>
-</div>        </section>
+                <div className="container">
+                    <div className="cta cta-default border-all margin-top-sm">
+                        <div className="row">
+                            <div className="col-sm-9 cta-text margin-top-xs">
+                                <h4>We’re serving up some pretty tasty features</h4>
+                            </div>
+                            <div className="col-sm-3 cta-button border-sm-left text-center">
+                                <Link className="btn btn-orange" to="/features"><span>See more features</span></Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>        
+            </section>
         );
     }
 }
--- src/components/home/testimonials.js
+++ src/components/home/testimonials.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
- 
+
+import img1 from '../../assets/img/career/foodcham-user-1.jpg';
+import img2 from '../../assets/img/career/foodcham-user-2.jpg';
+import img3 from '../../assets/img/career/foodcham-user-3.jpg';
+
+
+
 export default class Testimonials extends Component {
     render() {
         return (
-            <Carousel>
-                <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque, odio sit amet consequat semper, quam eros faucibus lorem, eget dignissim massa nisi in turpis. Sed mattis gravida nisi ut sodales. Aenean a quam nunc. Vivamus laoreet quam purus, non tincidunt elit sodales in. Maecenas posuere dolor at auctor semper. Sed dui lacus, rutrum sollicitudin pharetra quis, ullamcorper hendrerit libero. Phasellus at lacinia ante. Nullam tincidunt vehicula ex. Maecenas facilisis rutrum velit, id eleifend metus placerat et. Maecenas auctor, erat id volutpat facilisis, eros neque placerat enim, in varius tellus lorem sit amet erat.</p>
-                    {/* <p className="legend">Legend 1</p> */}
-                </div>
-                <div>
-                    {/* <img src="assets/2.jpeg" /> */}
-                    <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque, odio sit amet consequat semper, quam eros faucibus lorem, eget dignissim massa nisi in turpis. Sed mattis gravida nisi ut sodales. Aenean a quam nunc. Vivamus laoreet quam purus, non tincidunt elit sodales in. Maecenas posuere dolor at auctor semper. Sed dui lacus, rutrum sollicitudin pharetra quis, ullamcorper hendrerit libero. Phasellus at lacinia ante. Nullam tincidunt vehicula ex. Maecenas facilisis rutrum velit, id eleifend metus placerat et. Maecenas auctor, erat id volutpat facilisis, eros neque placerat enim, in varius tellus lorem sit amet erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque, odio sit amet consequat semper, quam eros faucibus lorem, eget dignissim massa nisi in turpis. Sed mattis gravida nisi ut sodales. Aenean a quam nunc. Vivamus laoreet quam purus, non tincidunt elit sodales in. Maecenas posuere dolor at auctor semper. Sed dui lacus, rutrum sollicitudin pharetra quis, ullamcorper hendrerit libero. Phasellus at lacinia ante. Nullam tincidunt vehicula ex. Maecenas facilisis rutrum velit, id eleifend metus placerat et. Maecenas auctor, erat id volutpat facilisis, eros neque placerat enim, in varius tellus lorem sit amet erat.</p>
-                </div>
-                <div>
-                    {/* <img src="assets/3.jpeg" /> */}
-                    <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque, odio sit amet consequat semper, quam eros faucibus lorem, eget dignissim massa nisi in turpis. Sed mattis gravida nisi ut sodales. Aenean a quam nunc. Vivamus laoreet quam purus, non tincidunt elit sodales in. Maecenas posuere dolor at auctor semper. Sed dui lacus, rutrum sollicitudin pharetra quis, ullamcorper hendrerit libero. Phasellus at lacinia ante. Nullam tincidunt vehicula ex. Maecenas facilisis rutrum velit, id eleifend metus placerat et. Maecenas auctor, erat id volutpat facilisis, eros neque placerat enim, in varius tellus lorem sit amet erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque, odio sit amet consequat semper, quam eros faucibus lorem, eget dignissim massa nisi in turpis. Sed mattis gravida nisi ut sodales. Aenean a quam nunc. Vivamus laoreet quam purus, non tincidunt elit sodales in. Maecenas posuere dolor at auctor semper. Sed dui lacus, rutrum sollicitudin pharetra quis, ullamcorper hendrerit libero. Phasellus at lacinia ante. Nullam tincidunt vehicula ex. Maecenas facilisis rutrum velit, id eleifend metus placerat et. Maecenas auctor, erat id volutpat facilisis, eros neque placerat enim, in varius tellus lorem sit amet erat.</p>
-                </div>
-            </Carousel>
+            <div>
+                <Carousel>
+                    <div>
+                        <img src={img1} className="img-responsive"/>
+                        <div className="col-md-6 col-md-offset-3">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque, odio sit amet consequat semper, quam eros faucibus lorem, eget dignissim massa nisi in turpis. Sed mattis gravida nisi ut sodales. Aenean a quam nunc. Vivamus laoreet quam purus, non tincidunt elit sodales in. Maecenas posuere dolor at auctor semper. Sed dui lacus, rutrum sollicitudin pharetra quis, ullamcorper hendrerit libero. Phasellus at lacinia ante. Nullam tincidunt vehicula ex. Maecenas facilisis rutrum velit, id eleifend metus placerat et. Maecenas auctor, erat id volutpat facilisis, eros neque placerat enim, in varius tellus lorem sit amet erat.</p>
+                        {/* <p className="legend">Legend 1</p> */}
+                        </div>
+                    </div>
+                    <div>
+                        <img src={img2} className="img-responsive"/>
+                        <div className="col-md-6 col-md-offset-3">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque, odio sit amet consequat semper, quam eros faucibus lorem, eget dignissim massa nisi in turpis. Sed mattis gravida nisi ut sodales. Aenean a quam nunc. Vivamus laoreet quam purus, non tincidunt elit sodales in. Maecenas posuere dolor at auctor semper. Sed dui lacus, rutrum sollicitudin pharetra quis, ullamcorper hendrerit libero. Phasellus at lacinia ante. Nullam tincidunt vehicula ex. Maecenas facilisis rutrum velit, id eleifend metus placerat et. Maecenas auctor, erat id volutpat facilisis, eros neque placerat enim, in varius tellus lorem sit amet erat.</p>
+                        {/* <p className="legend">Legend 1</p> */}
+                        </div>
+                    </div>
+                    <div>
+                        <img src={img3} className="img-responsive"/>
+                        <div className="col-md-6 col-md-offset-3">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque, odio sit amet consequat semper, quam eros faucibus lorem, eget dignissim massa nisi in turpis. Sed mattis gravida nisi ut sodales. Aenean a quam nunc. Vivamus laoreet quam purus, non tincidunt elit sodales in. Maecenas posuere dolor at auctor semper. Sed dui lacus, rutrum sollicitudin pharetra quis, ullamcorper hendrerit libero. Phasellus at lacinia ante. Nullam tincidunt vehicula ex. Maecenas facilisis rutrum velit, id eleifend metus placerat et. Maecenas auctor, erat id volutpat facilisis, eros neque placerat enim, in varius tellus lorem sit amet erat.</p>
+                        {/* <p className="legend">Legend 1</p> */}
+                        </div>
+                    </div>
+                </Carousel>
+            </div>
         );
     }
 };
